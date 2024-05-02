29df8b5c3d50e881d3ee5fe5cddeabca53a88843
trabelsi.ahmed.1@esprit.tn <trabelsi.ahmed.1@esprit.tn>
2024-05-02 20:22:31 +0200

    hi ala2235888

--- src/jobs/application/application.controller.ts
+++ src/jobs/application/application.controller.ts
@@ -82,7 +82,7 @@ private readonly logger = new ConsoleLogger(ApplicationController.name);
     // console.log('URL de l\'image:', cvData.selectedImage);
     console.log('Données reçues depuis le frontend:', cvData); 
     const browser = await puppeteer.launch({
-      executablePath: `C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe`,
+     
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     
